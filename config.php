076410e559f54e2d542e78c3b97b10e0c21310f3
Agbacoder <119514764+agbacoder@users.noreply.github.com>
2023-07-30 03:50:21 +0100

    Removed my details from the config file

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 include "resources/vendor/autoload.php";
 $gclient = new Google_Client();
-$gclient->setClientId("504383280868-ve3eo7pt95s7cidr3r4asl367usl7n31.apps.googleusercontent.com");
-$gclient->setClientSecret("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");
-$gclient->setApplicationName("Chat app regen");
-$gclient->setRedirectUri("http://localhost/chat%20app%20regen/includes/login_control.php");
+$gclient->setClientId("Your_client_id");
+$gclient->setClientSecret("Your_secret");
+$gclient->setApplicationName("App name");
+$gclient->setRedirectUri("Your_redirect_file");
 $gclient->addScope("https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/userinfo.email");
 
 $login_url = $gclient->createAuthUrl();
